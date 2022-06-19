5061ead2580425bca68fe751016958814991af25
김준한 <kjune1236@gmail.com>
2022-06-19 18:33:02 +0900

    html into templates

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def camera():
-    return render_template('./extractor/camera.html')
+    return render_template('camera.html')
 
 
 if __name__ == '__main__':
